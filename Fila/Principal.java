8ce32c4224e456b84cfd1d4c5a2d1a939e60fc03
pedrocks1000-beep <pedrocks1000@gmail.com>
2026-09-21 18:11:04 +0000

    Adicionando

--- Fila/Principal.java
+++ Fila/Principal.java
@@ -2,17 +2,21 @@ package Fila;
 
 public class Principal{
     public static void main(String[] args){
-        System.out.println("=== Testando a Fila ===");
         Fila<String> fila = new Fila<String>("Nomes dos alunos");
         fila.enfileirar("Peter");
         fila.enfileirar("Parker");
         fila.enfileirar("Mary Jane");
 
+        System.out.println(" Fila inicial ");
         fila.imprimeFila();
+
+        System.out.println(" Aluno removido ");
+        System.out.println(fila.desenfileirar());
 
         fila.enfileirar("Roberta");
         fila.enfileirar("Laiane");
 
+        System.out.println("Fila final ");
         fila.imprimeFila();
     }
 }
--- Pilha/Principal.java
+++ Pilha/Principal.java
@@ -2,16 +2,21 @@ package Pilha;
 
 public class Principal{
     public static void main(String[] args){
-        // Testando Pilha
-        System.out.println("=== Testando a Pilha ===");
-        Pilha<String> pilha = new Pilha<String>("Linguagens de Programação");
+       
+        System.out.println("Pilha");
+        Pilha<String> pilha = 
+        new Pilha<String>("Linguagens de Programação");
+        
         pilha.push("Java");
         pilha.push("Python");
         pilha.push("HTML");
         pilha.imprimePilha();
-        
-        pilha.pop();
 
+        pilha.pop();
+        pilha.imprimePilha();
+
+        pilha.push("C");
+        pilha.push("C++");
         pilha.imprimePilha();
     }
 }
