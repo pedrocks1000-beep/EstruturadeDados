76d6409c85014b513b4461bab89b4e563da52562
pedrocks1000-beep <pedrocks1000@gmail.com>
2026-09-21 19:43:43 +0000

    Adicionando correções

--- ListaDuplamenteEncadeada/No.java
+++ ListaDuplamenteEncadeada/No.java
@@ -4,17 +4,17 @@ public class No<T>{
     private T dado;
     private No<T> nextNo;
 
-    public  No(){
+    public No(){
         this(null, null);
     }
 
-    public  No(T dado){
+    public No(T dado){
         this(dado, null);
     }
 
-    public No(T dado, No<T> nextno){
+    public No(T dado, No<T> nextNo){   // corrigido: nextno → nextNo
         this.dado = dado;
-        this.nextNo = nextNo;
+        this.nextNo = nextNo;          // agora atribui o parâmetro ao campo
     }
 
     public void setDado(T dado){
@@ -32,8 +32,9 @@ public class No<T>{
     public No<T> getNextNo(){
         return this.nextNo;
     }
+
     @Override
-    public String toString(){
+     public String toString(){
     return dado.toString();
 }
 }
--- ListaDuplamenteEncadeada/Principal.java
+++ ListaDuplamenteEncadeada/Principal.java
@@ -2,12 +2,24 @@ package ListaDuplamenteEncadeada;
 
 public class Principal {
     public static void main(String[] args){
-        ListaDupla<String> lista = new ListaDupla<String>("Alunos");
+        ListaDupla<Produto> lista 
+        = new ListaDupla<Produto>("Produtos");
 
-        lista.addInicio("Tomate");
-        lista.addMeio("Rato", 1);
-        lista.addFinal("Hamburger");
+        lista.addInicio(new Produto("Teclado", 120.00));   
+        lista.addFinal(new Produto("Mouse", 60.50));       
+        lista.addMeio(new Produto("Monitor", 800.00), 1);  
 
+        System.out.println("Lista inicial ");
+        lista.imprimeLista();
+
+        Produto removido = lista.removeMeio(1);
+        System.out.println("removido:");
+        System.out.println(removido);
+
+        lista.addInicio(new Produto("Notebook", 3500.00));
+        lista.addFinal(new Produto("Webcam", 250.00));
+
+        System.out.println(" Lista final");
         lista.imprimeLista();
     }
 }
